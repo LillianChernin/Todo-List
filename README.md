fe770f34754d9f61266994310e6dab2ed49db324
Lillian Chernin <l.smith1654@gmail.com>
2018-01-11 21:20:18 -0600

    add basic files

---
+++ README.md
@@ -0,0 +1,2 @@
+# To Do List
+### Built with Node.js, Express, Mongoose, and MongoDB
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todo-list-fullstack');
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(3000, () => {
+  console.log("I am listening");
+})
+
+module.exports = app;
